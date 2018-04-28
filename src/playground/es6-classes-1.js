2f0c4dc3b6a5a8aa4ea243230ec2ffb44515f67c
Bowen <brentbwu@gmail.com>
2018-04-28 18:39:17 +1200

    parent class challenge

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,6 +12,7 @@ class Person {
         this.name = name;
         this.age = age;
     }
+    // method
     getGretting() {
         // return this.name;
         return 'Hi, I am ' + this.name;
@@ -22,11 +23,63 @@ class Person {
     }
 }
 
-const me = new Person('Andrew Mead','26');
+// sub class
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);// run father's constructor
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major; // flip it twice to get boolean outcome, like major is string, !'major' is boolean
+    }
+    // Override
+    getDescription() {
+        let description = super.getDescription();
+        
+        if(this.hasMajor()) {
+            description += ' Major is ' + this.major;
+        }
+        return description;
+    }
+}
 
+// Traveler -> Person
+// Add support for homeLocation
+// Override getGreating
+// 1. Hi, I am Andrew Mead. I'm visiting from Philadelphia
+// 2. Hi, I am Andrew Mead.
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    hasHomeLocation(){
+        return !!this.homeLocation;
+    }
+    getGretting() {
+        let gretting = super.getGretting();
+
+        if(this.hasHomeLocation()) {
+            return gretting += " I'm visiting from " + this.homeLocation;
+        }
+        return gretting;
+    }
+}
+
+const me = new Traveler('Andrew Mead','26','China');
+console.log(me);
+console.log(me.hasHomeLocation());
 console.log(me.getGretting());
-console.log(me.getDescription());
+// console.log(me);
+// console.log(me.hasMajor());
+// console.log(me.getGretting());
+// console.log(me.getDescription());
 
-const other = new Person();
+const other = new Traveler();
+console.log(other);
+console.log(other.hasHomeLocation());
 console.log(other.getGretting());
-console.log(other.getDescription());
+// console.log(other.hasMajor());
+// console.log(other.getDescription());
+// console.log(other.getGretting());

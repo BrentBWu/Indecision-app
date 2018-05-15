cd62be6c0d046efcb32bcfe0ac7715ce131000b1
Bowen <brentbwu@gmail.com>
2018-05-16 11:51:55 +1200

    First component

---
+++ src/app.js
@@ -0,0 +1,17 @@
+class Header extends React.Component{
+    render() {
+        return <p>This is from Header</p>
+    }
+}
+
+const jsx = (
+    <div>
+        <h1>Title</h1>
+        <Header />
+        <Header />
+        <Header />
+        <Header />
+    </div>
+);
+
+ReactDOM.render(jsx,document.getElementById('app'));

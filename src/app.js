ed3c5f6ced0453be8b1494d18843f45329306700
Bowen <brentbwu@gmail.com>
2018-05-16 17:43:11 +1200

    Component challenege

--- src/app.js
+++ src/app.js
@@ -1,16 +1,61 @@
 class Header extends React.Component{
     render() {
-        return <p>This is from Header</p>
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
+    }
+}
+
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do ?</button>
+            </div>
+        );
+    }
+}
+
+// Options -> Options component here
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <ol>
+                    <li>this is option</li>
+                    <li>this is option</li>
+                    <li>this is option</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+// const Add = () => {
+
+// };
+
+// AddOption -> AddOption component here
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <button /*onClick={Add}*/>Add Option</button>
+            </div>
+        );
     }
 }
 
 const jsx = (
     <div>
-        <h1>Title</h1>
         <Header />
         <Header />
-        <Header />
-        <Header />
+        <Action />
+        <Options />
+        <AddOption />
     </div>
 );
 

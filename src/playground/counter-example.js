805a0fa813aef86d2992bf3afdea92b3f3092211
Bowen <brentbwu@gmail.com>
2018-06-02 12:21:49 +1200

    state practice

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,44 +1,76 @@
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
+class Counter extends React.Component {
+    handleAddOne() {
+        console.log('handleAddOne');
+    }
 
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-};
+    handleMinusOne() {
+        console.log('handleMinusOne');
+    }
 
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-};
+    handleReset() {
+        console.log('handleReset');
+    }
+
+    
+    render() {
+        return (
+            <div>
+                <h1>Count</h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>reset</button>
+            </div>
+        );
+    }
+}
+
+// Create three methods: handleAddOne, handleMinusOne, handleReset
+// Use console.oh to print method name
+// Wire up onClick & bind in the constructor
+
+ReactDOM.render(<Counter />,document.getElementById('app'));
+
+// let count = 0;
+// const addOne = () => {
+//     count++;
+//     renderCounterApp();
+// };
+
+// const minusOne = () => {
+//     count--;
+//     renderCounterApp();
+// };
+
+// const reset = () => {
+//     count = 0;
+//     renderCounterApp();
+// };
 
 
 
 
-// Challenge
-// Make button "-1" - setup minusOne function and register - log "minusOne"
-// Make reset button "reset" - setup reset function - log "reset"
+// // Challenge
+// // Make button "-1" - setup minusOne function and register - log "minusOne"
+// // Make reset button "reset" - setup reset function - log "reset"
 
 
 
 
-const appRoot = document.getElementById("app");
+// const appRoot = document.getElementById("app");
 
 
 
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    );
+// const renderCounterApp = () => {
+//     const templateTwo = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={minusOne}>-1</button>
+//             <button onClick={reset}>reset</button>
+//         </div>
+//     );
 
-    ReactDOM.render(templateTwo,appRoot);  
-};
+//     ReactDOM.render(templateTwo,appRoot);  
+// };
 
-renderCounterApp();
+// renderCounterApp();
